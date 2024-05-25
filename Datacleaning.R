60da8a92a99222028634975121a366a7b9a2d826
NitsuhNega <nitsuhmengist@gmail.com>
2024-05-25 14:06:10 +0100

    fourth

--- Datacleaning.R
+++ Datacleaning.R
@@ -1,4 +1,4 @@
-* Cleaning sect1_hh_w1 using column hh_s1q02 that have a value of 1 (1 represent head)
+# Cleaning sect1_hh_w1 using column hh_s1q02 that have a value of 1 (1 represent head)
 sect1_hh_w1 <- subset(sect1_hh_w1, hh_s1q02 == 1)
 
 
